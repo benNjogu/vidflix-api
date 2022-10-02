28e221d5f29bc34fb3144c37c77f962df401617d
benNjogu <kamandemachafi@gmail.com>
2022-10-02 22:39:32 +0300

    GET all movie genres

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
 const genres = [
   { id: 1, genre: "Comedy" },
   { id: 2, genre: "Thriller" },
   { id: 3, genre: "Action" },
+  { id: 4, genre: "Animation" },
 ];
 
 app.get("/", (req, res) => {
   res.send("Hello world.");
 });
 
+app.get("/vidflix.com/api/genres", (req, res) => {
+  res.send(genres);
+});
+
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log("listening on port " + port));
+app.listen(port, () => console.log(`listening on port ${port}`));
