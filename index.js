149ee83a265ba1baa6573521ea239f3a88d5d9d8
benNjogu <kamandemachafi@gmail.com>
2022-10-11 17:56:57 +0300

    Adding Rentals API

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const home = require("./routes/home");
 const genres = require("./routes/genres");
 const customers = require("./routes/customers");
 const movies = require("./routes/movies");
+const rentals = require("./routes/rentals");
 const express = require("express");
 const app = express();
 
@@ -16,6 +17,7 @@ app.use("/", home);
 app.use("/vidflix.com/api/genres", genres);
 app.use("/vidflix.com/api/customers", customers);
 app.use("/vidflix.com/api/movies", movies);
+app.use("/vidflix.com/api/rentals", rentals);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on port ${port}`));
