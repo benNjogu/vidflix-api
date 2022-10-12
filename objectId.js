672333f6fe9a294738c3801186d982d19737741b
benNjogu <kamandemachafi@gmail.com>
2022-10-12 17:17:46 +0300

    removed unnecessary file

--- objectId.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const id = new mongoose.Types.ObjectId();
-
-console.log(id.getTimestamp()); //->2022-10-12T11:46:01.000Z
-
-const isValid = mongoose.Types.ObjectId.isValid("1234");
-console.log(isValid);//->false
