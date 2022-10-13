f1d60a314b5384244dff4d3d6f1df9e83f6a8e40
benNjogu <kamandemachafi@gmail.com>
2022-10-13 08:52:32 +0300

    Hashing a simple password

--- hash.js
+++ hash.js
@@ -2,7 +2,8 @@ const bcrypt = require("bcrypt");
 
 async function run() {
   const salt = await bcrypt.genSalt(10);
-  console.log(salt); //-> $2b$10$NUYY2zSGLod3BJPRFl9/l.
+  const hashed = await bcrypt.hash("1234", salt);
+  console.log(hashed); //-> $2b$10$gAUhtmLzpNekcQE/bxWNQeaX4EVUOK1l1zPuvNysqnpAitu5nO7SW
 }
 
 run();
