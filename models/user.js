af7e95ed3d5137bd9a6a641443740982be30a3f0
benNjogu <kamandemachafi@gmail.com>
2022-10-13 13:58:08 +0300

    Encapsulating logic in user model

--- models/user.js
+++ models/user.js
@@ -1,30 +1,36 @@
 const mongoose = require("mongoose");
 const Joi = require("joi");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 
-const User = mongoose.model(
-  "User",
-  new mongoose.Schema({
-    name: {
-      type: String,
-      required: true,
-      minLength: 3,
-      maxLength: 50,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      minLength: 5,
-      maxLength: 255,
-    },
-    password: {
-      type: String,
-      required: true,
-      minLength: 5,
-      maxLength: 1024,
-    },
-  })
-);
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 50,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    minLength: 5,
+    maxLength: 255,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 5,
+    maxLength: 1024,
+  },
+});
+
+userSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign({ _id: this._id }, config.get("jwtPrivateKey"));
+  return token;
+};
+
+const User = mongoose.model("User", userSchema);
 
 function validateGenre(user) {
   const schema = Joi.object({
