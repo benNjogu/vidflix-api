d081535a4c6855ba2b0e46897e34931a61f56936
benNjogu <kamandemachafi@gmail.com>
2022-10-14 13:29:01 +0300

    Extracting the DB logic

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const winston = require("winston");
 require("winston-mongodb");
 const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
-const mongoose = require("mongoose");
 const config = require("config");
 const express = require("express");
 const app = express();
 
 require("./startup/routes")(app);
+require("./startup/db")();
 
 process.on("uncaughtException", (ex) => {
-  winston.error(ex.message, error);
+  winston.error(ex.message);
   process.exit(1);
 });
 
@@ -34,11 +34,6 @@ if (!config.get("jwtPrivateKey")) {
   console.error("FATAL ERROR: jwtPrivateKey is not defined.");
   process.exit(1);
 }
-
-mongoose
-  .connect("mongodb://localhost/vidflix")
-  .then(() => console.log("Connected to the database..."))
-  .catch((err) => console.error("Could not connect to the database...", err));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on port ${port}`));
