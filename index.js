54d5b2e65dd04620c6dda40f730352a4d1094103
benNjogu <kamandemachafi@gmail.com>
2022-10-14 13:40:05 +0300

    Extracting the logging logic

--- index.js
+++ index.js
@@ -1,34 +1,12 @@
-require("express-async-errors");
-const winston = require("winston");
-require("winston-mongodb");
 const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
 const config = require("config");
 const express = require("express");
 const app = express();
 
+require("./startup/logging")();
 require("./startup/routes")(app);
 require("./startup/db")();
-
-process.on("uncaughtException", (ex) => {
-  winston.error(ex.message);
-  process.exit(1);
-});
-
-process.on("unhandledRejection", (ex) => {
-  winston.error(ex.message, error);
-  process.exit(1);
-});
-
-winston.add(new winston.transports.File({ filename: "logfile.log" }));
-winston.add(
-  new winston.transports.MongoDB({
-    db: "mongodb://localhost/vidflix",
-  })
-);
-
-const p = Promise.reject(new Error("Something failed miserably."));
-p.then(() => console.log("Done"));
 
 if (!config.get("jwtPrivateKey")) {
   console.error("FATAL ERROR: jwtPrivateKey is not defined.");
---
+++ startup/logging.js
@@ -0,0 +1,22 @@
+const winston = require("winston");
+require("winston-mongodb");
+require("express-async-errors");
+
+module.exports = function () {
+  process.on("uncaughtException", (ex) => {
+    winston.error(ex.message);
+    process.exit(1);
+  });
+
+  process.on("unhandledRejection", (ex) => {
+    winston.error(ex.message, error);
+    process.exit(1);
+  });
+
+  winston.add(new winston.transports.File({ filename: "logfile.log" }));
+  winston.add(
+    new winston.transports.MongoDB({
+      db: "mongodb://localhost/vidflix",
+    })
+  );
+};
