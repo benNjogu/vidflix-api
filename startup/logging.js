2ecca885a66400df32e53eb5daa49a93d7d6e0a5
benNjogu <kamandemachafi@gmail.com>
2022-10-14 14:01:21 +0300

    logging to the console

--- startup/logging.js
+++ startup/logging.js
@@ -13,7 +13,7 @@ module.exports = function () {
     process.exit(1);
   });
 
-  winston.add(new winston.transports.File({ filename: "logfile.log" }));
+  winston.add(new winston.transports.Console({ filename: "logfile.log" }));
   winston.add(
     new winston.transports.MongoDB({
       db: "mongodb://localhost/vidflix",
