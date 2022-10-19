99018a572844f7af635a3b0e9978f4ff9e19fd61
benNjogu <kamandemachafi@gmail.com>
2022-10-19 07:30:56 +0300

    Testing numbers

--- tests/testPlayground.test.js
+++ tests/testPlayground.test.js
@@ -1,3 +1,16 @@
-test("Our first test", () => {
-  throw new Error("Something failed.");
+const ground = require("../testPlayground");
+
+test("absolute - should return a positive number if the input is positive", () => {
+  const result = ground.absolute(1);
+  expect(result).toBe(1);
+});
+
+test("absolute - should return a positive number even if the input is negative", () => {
+  const result = ground.absolute(-1);
+  expect(result).toBe(1);
+});
+
+test("absolute - should return a zero if the input is null or zero", () => {
+  const result = ground.absolute(0);
+  expect(result).toBe(0);
 });
