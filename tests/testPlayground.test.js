24801ff7c87df527d98d186cf95ba6c46b0dee4c
benNjogu <kamandemachafi@gmail.com>
2022-10-19 09:33:22 +0300

    Grouping tests

--- tests/testPlayground.test.js
+++ tests/testPlayground.test.js
@@ -1,16 +1,19 @@
 const ground = require("../testPlayground");
 
-test("absolute - should return a positive number if the input is positive", () => {
-  const result = ground.absolute(1);
-  expect(result).toBe(1);
+describe("absolute", () => {
+  it("should return a positive number if the input is positive", () => {
+    const result = ground.absolute(1);
+    expect(result).toBe(1);
+  });
+
+  it("should return a positive number even if the input is negative", () => {
+    const result = ground.absolute(-1);
+    expect(result).toBe(1);
+  });
+
+  it("should return a zero if the input is null or zero", () => {
+    const result = ground.absolute(0);
+    expect(result).toBe(0);
+  });
 });
 
-test("absolute - should return a positive number even if the input is negative", () => {
-  const result = ground.absolute(-1);
-  expect(result).toBe(1);
-});
-
-test("absolute - should return a zero if the input is null or zero", () => {
-  const result = ground.absolute(0);
-  expect(result).toBe(0);
-});
