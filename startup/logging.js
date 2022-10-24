36febcac2b61de6d8f8f82b32b0ff6fa32ba884b
benNjogu <kamandemachafi@gmail.com>
2022-10-24 10:36:26 +0300

    Preparing the app

--- startup/logging.js
+++ startup/logging.js
@@ -1,5 +1,5 @@
 const winston = require("winston");
-require("winston-mongodb");
+//require("winston-mongodb");
 require("express-async-errors");
 
 module.exports = function () {
@@ -21,9 +21,9 @@ module.exports = function () {
     })
   );
   //winston.add(new winston.transports.File({ filename: "logfile.log" }));
-  winston.add(
+  /*winston.add(
     new winston.transports.MongoDB({
       db: "mongodb://localhost/vidflix",
     })
-  );
+  );*/
 };
