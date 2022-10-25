ee7419bcf31ca774095018120baf99e16c6cafba
benNjogu <kamandemachafi@gmail.com>
2022-10-25 20:28:35 +0300

    failing integration test

---
+++ tests/integration/genres.test.js
@@ -0,0 +1,19 @@
+const request = require("supertest");
+
+let server;
+
+describe("/vidflix/api/genres", () => {
+  beforeEach(() => {
+    server = require("../../index");
+  });
+  afterEach(() => {
+    server.close;
+  });
+
+  describe("GET /vidflix/api/genres", () => {
+    it("should return all genres", async () => {
+      const res = await request(server).get("/vidflix/api/genres");
+      expect(res.status).toBe(200);
+    });
+  });
+});
