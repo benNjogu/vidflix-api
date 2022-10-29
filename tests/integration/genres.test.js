1f62bbc7cdac66107cb67354ebab34289ab8827b
benNjogu <kamandemachafi@gmail.com>
2022-10-29 07:26:52 +0300

    Testing routes with parameters

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -25,4 +25,16 @@ describe("/vidflix/api/genres", () => {
       expect(res.body.some((g) => g.name === "genre2"));
     });
   });
+
+  describe("GET /:id", () => {
+    it("should return a genre if valid id is passed", async () => {
+      const genre = new Genre({ name: "genre1" });
+      await genre.save();
+
+      const res = await request(server).get("/vidflix/api/genres/" + genre._id);
+
+      expect(res.status).toBe(200);
+      expect(res.body).toHaveProperty("name", genre.name);
+    });
+  });
 });
