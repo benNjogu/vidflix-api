8dca9bab809840d8df49cfa1b95ef6e706e70afc
benNjogu <kamandemachafi@gmail.com>
2022-11-07 11:31:06 +0300

    Testing the response

--- tests/integration/returns.test.js
+++ tests/integration/returns.test.js
@@ -127,4 +127,25 @@ describe("/vidflix/api/returns", () => {
     const movieInDB = await Movie.findById(movieId);
     expect(movieInDB.numberInStock).toBe(movie.numberInStock + 1);
   });
+
+  it("should return the rental in the body of the response if the input is valid", async () => {
+    const res = await exec();
+
+    const rentalInDB = await Rental.findById(rental._id);
+    /*expect(res.body).toHaveProperty("dateOut");
+    expect(res.body).toHaveProperty("dateReturned");
+    expect(res.body).toHaveProperty("rentalFee");
+    expect(res.body).toHaveProperty("customer");
+    expect(res.body).toHaveProperty("movie");*/
+
+    expect(Object.keys(res.body)).toEqual(
+      expect.arrayContaining([
+        "dateOut",
+        "dateReturned",
+        "rentalFee",
+        "customer",
+        "movie",
+      ])
+    );
+  });
 });
